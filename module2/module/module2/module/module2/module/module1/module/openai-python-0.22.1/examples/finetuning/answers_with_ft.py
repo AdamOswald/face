0e69dda841ba744e5b232bd7653df359fb0f6756
Adam Oswald <adamoswald69420@gmail.com>
2022-10-13 13:50:06 -0400

    Squash 3 commits
    * 2ecc0cb by Adam Oswald <adamoswald69420@gmail.com>
    Squash 1 commits
    * f726138 by Adam Oswald <adamoswald69420@gmail.com>
    Squash 1 commits
    * 84a13c7 by Adam Oswald <adamoswald69420@gmail.com>
    Squash 1 commits

---
+++ module2/module/module2/module/module2/module/module1/module/openai-python-0.22.1/examples/finetuning/answers_with_ft.py
@@ -0,0 +1,2 @@
+# This code example has moved. You can now find it in the [OpenAI Cookbook](https://github.com/openai/openai-cookbook)
+# at [examples/fine-tuned_qa](https://github.com/openai/openai-cookbook/tree/main/examples/fine-tuned_qa)
