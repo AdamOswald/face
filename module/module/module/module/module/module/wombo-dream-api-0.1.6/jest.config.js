7485ee7133aa57b5ee14419b30b5c07dc3199449
Git X-Modules <support@tmatesoft.com>
2022-10-14 13:48:00 +0000

    Protecting root commit

--- module/module/module/module/module/module/wombo-dream-api-0.1.6/jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	maxWorkers: 1,
-	displayName: 'Wombo Dream API tests',
-};
