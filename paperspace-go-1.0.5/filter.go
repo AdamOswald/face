3148a22e9f83de08748e47163fb1e0825afab190
Git X-Modules <support@tmatesoft.com>
2022-10-14 17:31:35 +0000

    Protecting root commit

--- paperspace-go-1.0.5/filter.go
+++
@@ -1,7 +0,0 @@
-package paperspace
-
-type Filter struct {
-	Where map[string]interface{} `json:"where,omitempty"`
-	Limit int64                  `json:"limit,omitempty"`
-	Order string                 `json:"order,omitempty"`
-}
