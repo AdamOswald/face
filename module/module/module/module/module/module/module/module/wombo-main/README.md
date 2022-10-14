46ab26b4d7e7ad5da61eb4a42136d60d901780ea
Git X-Modules <support@tmatesoft.com>
2022-10-14 13:47:35 +0000

    Protecting root commit

--- module/module/module/module/module/module/module/module/wombo-main/README.md
+++
@@ -1,2 +0,0 @@
-# wombo
-A one day recreation of the [WOMBO.ai](https://wombo.ai) front-end apps using Expo and React Native
--- python-versions-3.11.0-rc.2-3058963384/tests/sources/python-config-output.py
+++
@@ -1,6 +0,0 @@
-import distutils.sysconfig
-import sysconfig
-
-from pprint import pprint
-pprint(sysconfig.get_config_vars())
-pprint(distutils.sysconfig.get_config_vars())
