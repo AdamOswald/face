1dbd46c521b5fdb59caabd93093d326706f5c681
Git X-Modules <support@tmatesoft.com>
2022-10-17 17:33:14 +0000

    Protecting root commit

--- generator-1.9.5/test/__mocks__/resolve-pkg.js
+++
@@ -1,8 +0,0 @@
-let resolvePkg = jest.genMockFromModule('resolve-pkg');
-
-resolvePkg.__resolvePkgValue = '';
-resolvePkg = jest.fn((path) => {
-  return resolvePkg.__resolvePkgValue;
-});
-
-module.exports = resolvePkg;
