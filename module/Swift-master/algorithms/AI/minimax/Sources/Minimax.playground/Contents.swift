87ffcd550439fe1d867976b1017872b156bd3110
Adam Oswald <adamoswald69420@gmail.com>
2022-10-19 13:37:31 -0400

    Delete module directory

--- module/Swift-master/algorithms/AI/minimax/Sources/Minimax.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-import PlaygroundSupport
-
-let boardSize = CGSize(width: 500, height: 550)
-let boardView = BoardView(frame: CGRect(origin: .zero, size: boardSize))
-
-PlaygroundPage.current.needsIndefiniteExecution = true
-PlaygroundPage.current.liveView = boardView
